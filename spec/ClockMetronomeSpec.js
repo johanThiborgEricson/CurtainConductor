04359e7111742af916385c9cf6d9e489410a0849
Johan Thiborg-Ericson <johan.thiborg.ericson@gmail.com>
2017-06-25 14:43:39 +0200

    Added unit-tests to ensure that the refactoring/new functionallity worked. Please don't tell Kent Beck.

--- spec/ClockMetronomeSpec.js
+++ spec/ClockMetronomeSpec.js
@@ -1,6 +1,6 @@
 describe("The clock-metronome", function() {
-  it("has a phase corresponding to the how much of the current second that " + 
-  "has passed", function() {
+  it("when bpm is 60 it has a phase corresponding to the how much of the " + 
+  "current second that has passed", function() {
     var clock = {
       now: function() {
         
@@ -16,6 +16,16 @@ describe("The clock-metronome", function() {
     expect(metronome.getPhase()).toBe(0.2);
   });
   
-  
+  it("can have different values for bpm", function() {
+    spyOn(Date, "now").and.returnValue(1000);
+    var metronome = new ClockMetronome(Date);
+    
+    metronome.bpm = 30;
+    expect(metronome.getPhase()).toBe(0.5);
+    metronome.bpm = 120;
+    expect(metronome.getPhase()).toBe(0);
+    metronome.bpm = 40;
+    expect(metronome.getPhase()).toBe(2/3);
+  });
   
 });
